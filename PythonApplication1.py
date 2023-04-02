dd7dc905ff323b659e0e8089ac2aca7c5e88e43f
Tommaso Vaccari <tomvac04@gmail.com>
2023-04-02 15:42:41 +0200

    v

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -12,7 +12,7 @@ pygame.display.set_caption("Minigames")
 #WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 PLAYER_WIDTH = 80
 PLAYER_HEIGHT = 100
-PLAYER_VEL = 5
+PLAYER_VEL = 8
 STAR_WIDTH = 80
 STAR_HEIGHT = 70
 STAR_VEL = 3
@@ -34,7 +34,7 @@ def draw(player, elapsed_time, stars,point,stop, gamestarter):
     time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
     WIN.blit(time_text, (10, 10))
     point_text = FONT.render(f"Points: {round(point)}", 1, "white")
-    WIN.blit(point_text, (600,10))
+    WIN.blit(point_text, (WIDTH-150,10))
     
     
     for star in stars:
@@ -62,28 +62,29 @@ def draw(player, elapsed_time, stars,point,stop, gamestarter):
 def main():
     gamestarter = False
     run = True
-    stop= 60
-    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,PLAYER_WIDTH, PLAYER_HEIGHT)
-    
-    
-    
-    clock = pygame.time.Clock()
-    
-    point = 0
-    vab = True 
+    vab = True
+    stop = 60
     star_add_increment = 2000
+    point = 0
     star_count = 0
     start_time=0
     stars = []
+    clock = new_func1() 
+    player = new_func()
+    
+    
+    
+    
 
     while run:
         
         star_count += clock.tick(60)
-        keys = pygame.key.get_pressed()
+        keys = new_func2()
+        
         if keys[pygame.K_SPACE] and vab == True :         #Tasto per far partire il gioco    
           gamestarter = True
           vab = False  
-          start_time=time.time()
+          start_time = new_func3()
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
@@ -101,7 +102,7 @@ def main():
             
             if star_count > star_add_increment:
                 for _ in range(3):
-                    star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                    star_x = new_func4()
                     star = pygame.Rect(star_x, -STAR_HEIGHT,STAR_WIDTH, STAR_HEIGHT)
                     stars.append(star)                   
                     
@@ -133,6 +134,26 @@ def main():
         draw(player, elapsed_time, stars, point,stop, gamestarter)
 
     pygame.quit()
+
+def new_func4():
+    star_x = random.randint(0, WIDTH - STAR_WIDTH)
+    return star_x
+
+def new_func3():
+    start_time=time.time()
+    return start_time
+
+def new_func2():
+    keys = pygame.key.get_pressed()
+    return keys
+
+def new_func1():
+    clock = pygame.time.Clock()
+    return clock
+
+def new_func():
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,PLAYER_WIDTH, PLAYER_HEIGHT)
+    return player
 
 
 if __name__ == "__main__":

a5a4c8b6662e95e56ab51f45f773d525dd903eba
Tommaso Vaccari <tomvac04@gmail.com>
2023-04-04 18:48:30 +0200

    v1

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -12,7 +12,7 @@ pygame.display.set_caption("Minigames")
 #WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 PLAYER_WIDTH = 80
 PLAYER_HEIGHT = 100
-PLAYER_VEL = 6
+PLAYER_VEL = 7
 STAR_WIDTH = 80
 STAR_HEIGHT = 70
 STAR_VEL = 3
@@ -29,20 +29,22 @@ st = pygame.transform.scale(pygame.image.load("start.jpeg"), (WIDTH, HEIGHT))
 
 
 def draw(player, elapsed_time, stars,point,stop, gamestarter):
-    WIN.blit(BG, (0, 0))
-    WIN.blit(pan,(player.x,player.y))
-    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
-    WIN.blit(time_text, (10, 10))
-    point_text = FONT.render(f"Points: {round(point)}", 1, "white")
-    WIN.blit(point_text, (WIDTH-150,10))
-    
+    if gamestarter == True :
+        WIN.blit(BG, (0, 0))
+        WIN.blit(pan,(player.x,player.y))
+        time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
+        WIN.blit(time_text, (10, 10))
+        point_text = FONT.render(f"Points: {round(point)}", 1, "white")
+        WIN.blit(point_text, (WIDTH-150,10))
+    else:
+        WIN.blit(st, (0,0))
     
     for star in stars:
-        WIN.blit(ful, star)
+        WIN.blit(ful, star)        
     if elapsed_time > stop :
         WIN.blit(BG1, (0, 0))
         finish_text = FONT.render(f"Time expired, good job you have done {round(point)} points ", 1, "black")
-        WIN.blit(finish_text, (0, 0))
+        WIN.blit(finish_text, (0, 0))                
     if gamestarter == False :
         WIN.blit(st, (0,0)) 
     pygame.display.update()    
@@ -63,7 +65,7 @@ def main():
     gamestarter = False
     run = True
     vab = True
-    stop = 60
+    stop = 10
     star_add_increment = 2000
     point = 0
     star_count = 0
@@ -75,21 +77,70 @@ def main():
     
     
     
+    
 
     while run:
-        
-        star_count += clock.tick(60)
-        keys = new_func2()
-        
-        if keys[pygame.K_SPACE] and vab == True :         #Tasto per far partire il gioco    
-          gamestarter = True
-          vab = False  
-          start_time = new_func3()
-        for event in pygame.event.get():
+        for event in pygame.event.get():    
                 if event.type == pygame.QUIT:
                     run = False
-                    break                   #Tasto quit
-        if  gamestarter == True and elapsed_time < stop:
+                    
+                else:
+                    run = True
+        keys = new_func2(player)    
+        if keys[pygame.K_SPACE] and vab == True :         #Tasto per far partire il gioco    
+            gamestarter = True
+            vab = False  
+            start_time = new_func3()
+        elapsed_time = time.time() - start_time
+        
+        gioco_1(star_count,clock,gamestarter,elapsed_time,stop,stars,star_add_increment,player)
+        draw(player, elapsed_time, stars, point,stop, gamestarter)
+
+    pygame.quit()
+
+
+
+
+
+
+
+
+
+
+
+
+def new_func5(star_x):
+    star = pygame.Rect(star_x, -STAR_HEIGHT,STAR_WIDTH, STAR_HEIGHT)
+    #pygame.time.delay(250)
+    return star
+
+def new_func4():
+    star_x = random.randint(0, WIDTH - STAR_WIDTH)
+    return star_x
+
+def new_func3():
+    start_time=time.time()
+    return start_time
+
+def new_func2(player):
+    keys = pygame.key.get_pressed()
+    return keys
+
+def new_func1():
+    clock = pygame.time.Clock()
+    return clock
+
+def new_func():
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,PLAYER_WIDTH, PLAYER_HEIGHT)
+    return player
+
+def gioco_1(star_count,clock,gamestarter,elapsed_time,stop,stars,star_add_increment,player):
+    
+    star_count += clock.tick(60)
+    
+        
+    
+    if  gamestarter == True and elapsed_time < stop:    
             for star in stars[:]:
                 star.y += STAR_VEL
                 if star.y > HEIGHT-50:
@@ -117,51 +168,39 @@ def main():
 
 
 
-            keys = new_func2()
+            keys = new_func2(player)
             if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
                 player.x -= PLAYER_VEL
             if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
                 player.x += PLAYER_VEL
-        elapsed_time = time.time() - start_time
-        
-            
-            
-
-           
-
-        
-        
 
 
-        draw(player, elapsed_time, stars, point,stop, gamestarter)
-
-    pygame.quit()
-
-def new_func5(star_x):
-    star = pygame.Rect(star_x, -STAR_HEIGHT,STAR_WIDTH, STAR_HEIGHT)
-    #pygame.time.delay(250)
-    return star
-
-def new_func4():
-    star_x = random.randint(0, WIDTH - STAR_WIDTH)
-    return star_x
-
-def new_func3():
-    start_time=time.time()
-    return start_time
-
-def new_func2():
-    keys = pygame.key.get_pressed()
-    return keys
-
-def new_func1():
-    clock = pygame.time.Clock()
-    return clock
-
-def new_func():
-    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,PLAYER_WIDTH, PLAYER_HEIGHT)
-    return player
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+    
+

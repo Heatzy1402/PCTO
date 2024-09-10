58e53acabd1f1f8d8390a5e89006d8d377bec1d4
T-vaccari <05-gesto-follemente@icloud.com>
2024-09-10 11:30:20 +0200

    Removed

--- MissionEnvironment.py
+++ MissionEnvironment.py
@@ -317,13 +317,7 @@ def gioco_2(WIDTH,HEIGTH,punteggio_tot) :
 
 
 
-#def new_func5(star_x):
- #   star = pygame.Rect(star_x, -STAR_HEIGHT,STAR_WIDTH, STAR_HEIGHT)
- #   return star
 
-#def new_func4():
- #   star_x = random.randint(0, WIDTH - STAR_WIDTH)
- #   return star_x
 
 def tasti():
     keys = pygame.key.get_pressed()
